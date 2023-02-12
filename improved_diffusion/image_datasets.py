c58e3b22ea3f408571aaa28bdf47aeaefcafcbeb
Michael Saxon <xmichaelsaxon@gmail.com>
2023-02-11 18:20:12 -0800

    changed logging

--- improved_diffusion/image_datasets.py
+++ improved_diffusion/image_datasets.py
@@ -101,7 +101,7 @@ class ImageDataset(Dataset):
         arr = arr.astype(np.float32) / 127.5 - 1
 
         out_dict = {}
+        out_dict["idx"] = idx
         if self.local_classes is not None:
             out_dict["y"] = np.array(self.local_classes[idx], dtype=np.int64)
-            out_dict["idx"] = idx
         return np.transpose(arr, [2, 0, 1]), out_dict
