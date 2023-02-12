8d0bafac01f06cf35f85ab80ef149db5d9902d45
Michael Saxon <xmichaelsaxon@gmail.com>
2023-02-11 17:32:17 -0800

    changed logging

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -72,6 +72,7 @@ def create_argparser():
         resume_checkpoint="",
         use_fp16=False,
         fp16_scale_growth=1e-3,
+        log_dir="/local/home/saxon/improved-diffusion-logs/"
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
